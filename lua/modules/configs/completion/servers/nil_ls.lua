90c871db5f7b0879e1e465a3d2ef24b96edd5ed5
charliie-dev <mail@charliie.dev>
2026-01-27 01:41:33 +0800

    perf(lsp): disable all semantic token for faster speed

--- lua/modules/configs/completion/servers/nil_ls.lua
+++ lua/modules/configs/completion/servers/nil_ls.lua
@@ -1,6 +1,11 @@
 -- https://github.com/neovim/nvim-lspconfig/blob/master/lua/lspconfig/configs/nil_ls.lua
 return {
 	single_file_support = true,
+	on_init = function(client, _)
+		if client.server_capabilities then
+			client.server_capabilities.semanticTokensProvider = nil
+		end
+	end,
 	settings = {
 		["nil"] = {
 			testSetting = 42,
