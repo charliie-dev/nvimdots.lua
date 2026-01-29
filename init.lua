16a469326371f96df5027cfd1fe9c70097894182
charliie-dev <mail@charliie.dev>
2026-01-29 18:28:22 +0800

    fix: source LUA paths generated by home-manager

--- init.lua
+++ init.lua
@@ -1,5 +1,8 @@
 if not vim.g.vscode then
 	-- only needed when managing nvim dependencies from home-manager, otherwise comment this line
 	local has_generated, hm_generated = pcall(require, "nix-generated")
+	if has_generated then
+		hm_generated()
+	end
 	require("core")
 end
