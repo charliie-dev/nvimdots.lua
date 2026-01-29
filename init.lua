d17bcf8eede6d3f1cd1eaeabec171e964cadb5bc
charliie-dev <mail@charliie.dev>
2026-01-29 11:22:11 +0800

    perf(nix): update LUA settings managed by home-manager

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 if not vim.g.vscode then
-	require("home-manager") -- only needed when managing nvim dependencies from home-manager, otherwise comment this line
+	-- only needed when managing nvim dependencies from home-manager, otherwise comment this line
+	local has_generated, hm_generated = pcall(require, "nix-generated")
 	require("core")
 end
