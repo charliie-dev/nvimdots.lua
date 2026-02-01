18251a492625913406846249f7b78f0de236ee9d
charliie-dev <mail@charliie.dev>
2026-02-01 10:58:16 +0800

    chore: fix init.lua

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 if not vim.g.vscode then
 	-- only needed when managing nvim dependencies from home-manager, otherwise comment this line
-	local has_generated, hm_generated = pcall(require, "nix-generated")
+	local has_generated, _ = pcall(require, "nix-generated")
 	if has_generated then
-		hm_generated()
+		require("nix-generated")
 	end
 	require("core")
 end
