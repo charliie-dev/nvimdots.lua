6a8a6487717e0ca14bf5193e9c1953ba58db0eed
charliie-dev <mail@charliie.dev>
2026-03-25 21:26:31 +0800

    feat(snacks): configure picker with flash integration and frecency

--- lua/modules/configs/ui/snacks.lua
+++ lua/modules/configs/ui/snacks.lua
@@ -128,12 +128,46 @@ return function()
 		},
 		terminal = { enabled = true },
 		quickfile = { enabled = true },
+		picker = {
+			enabled = true,
+			sources = {
+				smart = { matcher = { frecency = true } },
+			},
+			win = {
+				input = {
+					keys = {
+						["<a-s>"] = { "flash", mode = { "n", "i" } },
+						["s"] = { "flash" },
+					},
+				},
+			},
+			actions = {
+				flash = function(picker)
+					require("flash").jump({
+						pattern = "^",
+						label = { after = { 0, 0 } },
+						search = {
+							mode = "search",
+							exclude = {
+								function(win)
+									return vim.bo[vim.api.nvim_win_get_buf(win)].filetype
+										~= "snacks_picker_list"
+								end,
+							},
+						},
+						action = function(match)
+							local idx = picker.list:row2idx(match.pos[1])
+							picker.list:_move(idx, true, true)
+						end,
+					})
+				end,
+			},
+		},
 		-- Disable unused modules
 		animate = { enabled = false },
 		dim = { enabled = false },
 		explorer = { enabled = false },
 		input = { enabled = false },
-		picker = { enabled = true },
 		scope = { enabled = false },
 		statuscolumn = { enabled = false },
 		toggle = { enabled = false },
