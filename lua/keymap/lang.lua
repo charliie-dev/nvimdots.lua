5e9e94ff7912d11e37abb793f483ba49b52907f3
charliie-dev <mail@charliie.dev>
2026-03-26 00:09:48 +0800

    refactor(keymap): remove old plugin-based keymap files

--- lua/keymap/lang.lua
+++
@@ -1,7 +0,0 @@
-local set = vim.keymap.set
-
--- Plugin: markview.nvim
-set("n", "<F1>", "<Cmd>Markview toggle<CR>", { silent = true, desc = "tool: toggle markdown preview within nvim" })
-
--- Plugin: MarkdownPreview
-set("n", "<F12>", "<Cmd>MarkdownPreviewToggle<CR>", { silent = true, desc = "tool: Preview markdown" })
