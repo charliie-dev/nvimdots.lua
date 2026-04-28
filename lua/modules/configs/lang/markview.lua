db5a26b5f48a58e4ea02db7febc8de569eceb00c
charliie-dev <mail@charliie.dev>
2026-04-28 21:40:53 +0800

    fix(markview): update on_enable callback to iterate wins array

--- lua/modules/configs/lang/markview.lua
+++ lua/modules/configs/lang/markview.lua
@@ -4,9 +4,11 @@ return function()
 			modes = { "n", "no", "c" },
 			hybrid_modes = { "n" },
 			callbacks = {
-				on_enable = function(_, win)
-					vim.wo[win].conceallevel = 2
-					vim.wo[win].concealcursor = "c"
+				on_enable = function(_, wins)
+					for _, win in ipairs(wins) do
+						vim.wo[win].conceallevel = 2
+						vim.wo[win].concealcursor = "c"
+					end
 				end,
 			},
 		},
