701c73f7066e371b23b23ea3c773f36e72fee7e0
charliie-dev <mail@charliie.dev>
2026-04-28 21:18:37 +0800

    chore(ts-textobjects): add explicit lookbehind and include_surrounding_whitespace defaults

--- lua/modules/configs/editor/ts-textobjects.lua
+++ lua/modules/configs/editor/ts-textobjects.lua
@@ -2,6 +2,8 @@ return function()
 	require("modules.utils").load_plugin("nvim-treesitter-textobjects", {
 		select = {
 			lookahead = true,
+			lookbehind = false,
+			include_surrounding_whitespace = false,
 			selection_modes = {
 				["@parameter.outer"] = "v", -- charwise
 				["@function.outer"] = "V", -- linewise
