f45350ea863237895576b986037cc3fd3e5d390c
charliie-dev <mail@charliie.dev>
2026-04-28 21:04:20 +0800

    chore(blink): set explicit show_without_* ghost_text defaults

--- lua/modules/configs/completion/blink.lua
+++ lua/modules/configs/completion/blink.lua
@@ -118,7 +118,9 @@ local opts = {
 		ghost_text = {
 			enabled = true,
 			show_with_selection = true,
+			show_without_selection = false,
 			show_with_menu = true,
+			show_without_menu = true,
 		},
 	},
 
