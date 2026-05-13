9be19a3598327933d9761b5477843a029c7063fa
charliie-dev <mail@charliie.dev>
2026-05-13 09:48:51 +0800

    fix(cmp): reduce blink-ripgrep freeze in complex directories
    Signed-off-by: charliie-dev <mail@charliie.dev>

--- lua/modules/configs/completion/blink.lua
+++ lua/modules/configs/completion/blink.lua
@@ -178,12 +178,13 @@ local opts = {
 				---@module "blink-ripgrep"
 				---@type blink-ripgrep.Options
 				opts = {
-					prefix_min_len = 2,
+					prefix_min_len = 3,
 					backend = {
-						use = "ripgrep",
+						use = "gitgrep-or-ripgrep",
 						ripgrep = {
-							context_size = 5,
-							max_filesize = "1M",
+							max_filesize = "200K",
+							context_size = 3,
+							additional_rg_options = { "--max-count=5" },
 						},
 					},
 				},
