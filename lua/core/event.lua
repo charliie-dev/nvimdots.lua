86d31fa97666d2a10e0baa5ff58cfa3667450810
charliie-dev <mail@charliie.dev>
2026-08-18 10:15:40 +0800

    feat(event): center cursor at last edit

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -55,13 +55,13 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	end,
 })
 
--- Autojump to last edit
+-- Autojump to and center the last edit
 vim.api.nvim_create_autocmd("BufReadPost", {
 	callback = function()
 		local mark = vim.api.nvim_buf_get_mark(0, '"')
 		local lcount = vim.api.nvim_buf_line_count(0)
-		if mark[1] > 0 and mark[1] <= lcount then
-			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		if mark[1] > 0 and mark[1] <= lcount and pcall(vim.api.nvim_win_set_cursor, 0, mark) then
+			vim.cmd("normal! zz")
 		end
 	end,
 })
